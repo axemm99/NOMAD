1d4c1aa58e9bfa718a2fa586b414d642bf3bd38f
axemm99 <inesmm1999@gmail.com>
2018-06-21 16:33:56 +0100

    estatisticas

--- js/statistics.js
+++ js/statistics.js
@@ -44,6 +44,29 @@ function addLoadEvent(func) {
                     </div>`
         barBookCapacity.innerHTML = strHtml
     }
+
+    /* total books available */
+    function percentageBookAvailable() {
+        let totalBooks = books.length
+        let totalAvailable = 0
+        let percentage = 0
+        let strHtml = ""
+
+            totalAvailable = totalBooks - requests.length
+        
+
+        percentage =  (totalAvailable * 100) / totalBooks
+        let percentageRound = Math.round(percentage)
+
+        strHtml += `<div class='col-md-12'>
+                        <h6 class='inline col-md-6'>Livros Disponíveis</h6>
+                        <p class='inline' style='float: right;'>${percentageRound}%</p>
+                    </div>
+                    <div class='progress'>
+                        <div class='progress-bar progress-bar-blue' role='progressbar' aria-valuenow='${percentageRound}' aria-valuemin='0' aria-valuemax='100' style='width: ${percentageRound}%'></div>
+                    </div>`
+        barBookAvailable.innerHTML = strHtml
+    }
 //
 
 
@@ -65,6 +88,81 @@ function addLoadEvent(func) {
                     <h3>${totalUsers}</h3>`
 
         counterUsers.innerHTML = strHtml
+    }
+
+    /* total books */
+    function counterTotalBooks() {
+        let totalBooks = 0
+        let strHtml = ""
+
+        for (let i = 0; i < books.length; i++) {
+            totalBooks++                        
+        }
+
+        strHtml += `<h6>Livros</h6>
+                    <h3>${totalBooks}</h3>`
+
+        counterBooks.innerHTML = strHtml
+    }
+
+    /* total libraries */
+    function counterTotalLibraries() {
+        let totalLibraries = 0
+        let strHtml = ""
+
+        for (let i = 0; i < libraries.length; i++) {
+            totalLibraries++                        
+        }
+
+        strHtml += `<h6>Bibliotecas</h6>
+                    <h3>${totalLibraries}</h3>`
+
+        counterLibraries.innerHTML = strHtml
+    }
+
+    /* total categories */
+    function counterTotalCategories() {
+        let totalCategories = 0
+        let strHtml = ""
+
+        for (let i = 0; i < categories.length; i++) {
+            totalCategories++                        
+        }
+
+        strHtml += `<h6>Categorias</h6>
+                    <h3>${totalCategories}</h3>`
+
+        counterCategories.innerHTML = strHtml
+    }
+
+    /* total tags */
+    function counterTotalTags() {
+        let totalTags = 0
+        let strHtml = ""
+
+        for (let i = 0; i < tags.length; i++) {
+            totalTags++                        
+        }
+
+        strHtml += `<h6>Tags</h6> <i class="fas fa-tag"></i>
+                    <h3>${totalTags}</h3>`
+
+        counterTags.innerHTML = strHtml
+    }
+
+    /* total requests */
+    function counterTotalRequests() {
+        let totalRequests = 0
+        let strHtml = ""
+
+        for (let i = 0; i < requests.length; i++) {
+            totalRequests++                        
+        }
+
+        strHtml += `<h6>Requisições</h6>
+                    <h3>${totalRequests}</h3>`
+
+        counterRequests.innerHTML = strHtml
     }
 
 
@@ -104,9 +202,15 @@ addLoadEvent(function() {
 
         /* counters */
         let counterUsers = document.getElementById("counterUsers")
+        let counterBooks = document.getElementById("counterBooks")
+        let counterLibraries = document.getElementById("counterLibraries")
+        let counterCategories = document.getElementById("counterCategories")
+        let counterTags = document.getElementById("counterTags")
+        let counterRequests = document.getElementById("counterRequests")
 
         /* bars */
         let barBookCapacity = document.getElementById("barBookCapacity")
+        let barBookAvailable = document.getElementById("barBookAvailable")
 
 
         let inputStatistictsCategory = document.getElementById("inputStatistictsCategory")
@@ -127,9 +231,15 @@ addLoadEvent(function() {
 
         /* counter */
         counterTotalUsers()
+        counterTotalBooks()
+        counterTotalLibraries()
+        counterTotalCategories()
+        counterTotalTags()
+        counterTotalRequests()
 
         /* percentages */
         percentageBookCapacity()
+        percentageBookAvailable()
         /*percentageBooks()
         addCategories()
         addTags()*/
